9b872bdc8950f53fa40b1885dbbc86ed429740d9
d-laub <dlaub@ucsd.edu>
2023-05-20 11:59:47 -0700

    feat: expose cast_seqs at top level.

--- seqpro/__init__.py
+++ seqpro/__init__.py
@@ -1,11 +1,12 @@
 from ._alphabets import ALPHABETS, NucleotideAlphabet
-from ._analyzers import count_kmers_seq, gc_content, length, nucleotide_content
+from ._analyzers import gc_content, length, nucleotide_content
 from ._cleaners import remove_N_seqs, remove_only_N_seqs, sanitize_seq, sanitize_seqs
 from ._encoders import ohe, ohe_to_bytes, pad_seqs
 from ._modifiers import k_shuffle, reverse_complement, shuffle
-from ._utils import random_seq, random_seqs
+from ._utils import cast_seqs, random_seq, random_seqs
 
 __all__ = [
+    "cast_seqs",
     "count_kmers_seq",
     "gc_content",
     "length",
