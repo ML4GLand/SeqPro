enh: start work on normalization function. fix: typos.

seqpro/_analyzers.py
@@ -20,8 +20,8 @@ def length(seqs: Union[str, List[str]]) -> NDArray[np.integer]:
     np.array
         Array containing the length of each sequence.
     """
-    _seqs = cast_seqs(seqs)
-    return (_seqs != b"").sum(-1)
+    seqs = cast_seqs(seqs)
+    return (seqs != b"").sum(-1)
 
 
 def gc_content(

seqpro/_encoders.py
@@ -1,7 +1,6 @@
-from typing import List, Literal, Optional, Union, cast
+from typing import Literal, Optional, Union, cast
 
 import numpy as np
-import torch
 from numpy.typing import NDArray
 
 from ._numba import gufunc_char_idx, gufunc_pad_both, gufunc_pad_left
@@ -16,14 +15,16 @@ def pad_seqs(
     length: Optional[int] = None,
     length_axis: Optional[int] = None,
 ) -> NDArray[Union[np.bytes_, np.uint8]]:
-    """_summary_
+    """Pad (or truncate) sequences on either the left, right, or both sides.
 
     Parameters
     ----------
     seqs : Iterable[str]
     pad : Literal["left", "both", "right"]
         How to pad. If padding on both sides and an odd amount of padding is needed, 1
-        more pad value will be on the right side.
+        more pad value will be on the right side. Similarly for truncating, if an odd
+        amount length needs to be truncated, 1 more character will be truncated from the
+        right side.
     pad_val : str, optional
         Single character to pad sequences with. Needed for string input. Ignored for OHE
         sequences.
@@ -77,7 +78,14 @@ def pad_seqs(
         if length_diff == 0:
             return seqs
         elif length_diff > 0:  # longer than needed, truncate
-            seqs = array_slice(seqs, length_axis, 0, length)
+            if pad == "left":
+                seqs = array_slice(seqs, length_axis, -length)
+            elif pad == "both":
+                seqs = array_slice(
+                    seqs, length_axis, length_diff // 2, -length_diff // 2
+                )
+            else:
+                seqs = array_slice(seqs, length_axis, stop=length)
         else:  # shorter than needed, pad
             pad_arr_shape = (
                 *seqs.shape[:length_axis],

seqpro/_modifiers.py
@@ -1,4 +1,5 @@
-from typing import Optional, Tuple, Union, cast
+from enum import Enum
+from typing import Literal, Optional, Tuple, Union, cast
 
 import numpy as np
 import ushuffle
@@ -120,8 +121,7 @@ def bin_coverage(
     coverage_array : NDArray
     bin_width : int
         Width of the windows to sum over. Must be an even divisor of the length
-        of the coverage array. If not, raises an error. The length dimension is
-        assumed to be the second dimension.
+        of the coverage array. If not, raises an error.
     length_axis: int
     normalize : bool, default False
         Whether to normalize by the length of the bin.
@@ -169,15 +169,15 @@ def jitter(
     Returns
     -------
     arrays
-        Jittered arrays. Will have a new length equal to length - 2*max_jitter.
+        Jittered arrays. Each will have a new length equal to length - 2*max_jitter.
     """
     if isinstance(jitter_axes, int):
         jitter_axes = (jitter_axes,)
 
     destination_axes = list(range(-len(jitter_axes) - 1, 0))
-    arrays = [
+    arrays = tuple(
         np.moveaxis(a, [*jitter_axes, length_axis], destination_axes) for a in arrays
-    ]
+    )
 
     jitter_axes_shape = arrays[0].shape[-len(destination_axes) : -1]
     for arr in arrays:
@@ -224,3 +224,47 @@ def random_seqs(
     """
     rng = np.random.default_rng(seed)
     return rng.choice(alphabet.array, size=shape)
+
+
+class NormalizationMethod(str, Enum):
+    CPM = "CPM"
+    CPKM = "CPKM"
+
+
+def normalize_coverage(
+    coverage: NDArray,
+    method: Union[Literal["CPM", "CPKM"], NormalizationMethod],
+    total_counts: Union[int, NDArray],
+    length_axis: int,
+):
+    """Normalize an array of coverage. Note that whether this corresponds to the
+    conventional definitions of CPM, RPKM, and FPKM depends on how the underlying
+    coverage was quantified. If the coverage is for reads, then CPKM = RPKM. If it is
+    for fragments, then CPKM = FPKM.
+
+    Parameters
+    ----------
+    coverage : NDArray
+        Array of coverage.
+    method : Union[Literal['CPM', 'CPKM'], NormalizationMethod]
+        Normalization method, either CPM (counts per million) or CPKM (counts per
+        kilobase per million).
+    total_counts : Union[int, NDArray]
+        The total number of reads or fragments from the experiment.
+    length_axis : int
+
+    Returns
+    -------
+    NDArray
+        Array of normalized coverage.
+    """
+    method = NormalizationMethod(method)
+
+    length = coverage.shape[length_axis]
+
+    if method is NormalizationMethod.CPM:
+        coverage = coverage * 1e9 / total_counts
+    elif method is NormalizationMethod.CPKM:
+        coverage = coverage * 1e9 / total_counts / length
+
+    return coverage

seqpro/_numba.py
@@ -5,7 +5,7 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-@nb.guvectorize(["(u1[:], u1[:])"], "(n)->(n)", target="parallel", nopython=True)
+@nb.guvectorize(["(u1[:], u1[:])"], "(n)->(n)", target="parallel", cache=True)
 def gufunc_pad_left(
     seq: NDArray[np.uint8], res: Optional[NDArray[np.uint8]] = None
 ) -> NDArray[np.uint8]:  # type: ignore
@@ -14,7 +14,7 @@ def gufunc_pad_left(
         res[(i + shift) % len(seq)] = seq[i]  # type: ignore
 
 
-@nb.guvectorize(["(u1[:], u1[:])"], "(n)->(n)", target="parallel", nopython=True)
+@nb.guvectorize(["(u1[:], u1[:])"], "(n)->(n)", target="parallel", cache=True)
 def gufunc_pad_both(
     seq: NDArray[np.uint8], res: Optional[NDArray[np.uint8]] = None
 ) -> NDArray[np.uint8]:  # type: ignore
@@ -23,7 +23,7 @@ def gufunc_pad_both(
         res[(i + shift) % len(seq)] = seq[i]  # type: ignore
 
 
-@nb.guvectorize(["(u1, u1[:], u1[:])"], "(),(n)->(n)", target="parallel", nopython=True)
+@nb.guvectorize(["(u1, u1[:], u1[:])"], "(),(n)->(n)", target="parallel", cache=True)
 def gufunc_ohe(
     char: Union[np.uint8, NDArray[np.uint8]],
     alphabet: NDArray[np.uint8],
@@ -33,7 +33,7 @@ def gufunc_ohe(
         res[i] = np.uint8(alphabet[i] == char)  # type: ignore
 
 
-@nb.guvectorize(["(u1[:], intp[:])"], "(n)->()", target="parallel", nopython=True)
+@nb.guvectorize(["(u1[:], intp[:])"], "(n)->()", target="parallel", cache=True)
 def gufunc_ohe_char_idx(
     seq: NDArray[np.uint8],
     res: Optional[NDArray[np.intp]] = None,
@@ -59,9 +59,7 @@ def gufunc_ohe_char_idx(
             res[0] = i  # type: ignore
 
 
-@nb.guvectorize(
-    ["(u1, u1[:], intp[:])"], "(),(n)->()", target="parallel", nopython=True
-)
+@nb.guvectorize(["(u1, u1[:], intp[:])"], "(),(n)->()", target="parallel", cache=True)
 def gufunc_char_idx(
     seq: NDArray[np.uint8],
     alphabet: NDArray[np.uint8],
@@ -77,7 +75,7 @@ def gufunc_char_idx(
     ["(u1[:], u1[:, :], u1[:], u1[:])"],
     "(k),(j,k),(j)->()",
     target="parallel",
-    nopython=True,
+    cache=True,
 )
 def gufunc_translate(
     seq: NDArray[np.uint8],
@@ -103,7 +101,7 @@ def gufunc_translate(
             res[0] = aminos_acids[i]  # type: ignore
 
 
-@nb.guvectorize("(n),(),()->(n)", nopython=True)
+@nb.guvectorize("(n),(),()->(n)", target="parallel", cache=True)
 def gufunc_jitter_helper(
     arr: NDArray,
     start: Union[int, NDArray[np.integer]],

seqpro/_utils.py
@@ -3,8 +3,10 @@ from typing import List, Optional, TypeVar, Union, cast, overload
 import numpy as np
 from numpy.typing import NDArray
 
-StrSeqType = Union[str, List[str], NDArray[Union[np.str_, np.bytes_]]]
-SeqType = Union[str, List[str], NDArray[Union[np.str_, np.bytes_, np.uint8]]]
+StrSeqType = Union[str, List[str], NDArray[Union[np.str_, np.object_, np.bytes_]]]
+SeqType = Union[
+    str, List[str], NDArray[Union[np.str_, np.object_, np.bytes_, np.uint8]]
+]
 
 
 @overload
@@ -28,7 +30,7 @@ def cast_seqs(seqs: SeqType) -> NDArray[Union[np.bytes_, np.uint8]]:
 
     Parameters
     ----------
-    seqs : str, (nested) list[str], ndarray[str, bytes, uint8]
+    seqs : str, (nested) list[str], ndarray[str, object (Python strings), bytes, uint8]
 
     Returns
     -------
@@ -52,12 +54,14 @@ def _check_axes(
     """Raise errors if length_axis or ohe_axis is missing when they're needed. Pass
     False to corresponding axis to not check for it.
 
-    - Any ndarray => length axis required.
-    - Any OHE array => length and OHE axis required.
+    - ndarray with itemsize == 1 => length axis required.
+    - OHE array => length and OHE axis required.
     """
     # bytes or OHE
     if length_axis is None and isinstance(seqs, np.ndarray) and seqs.itemsize == 1:
-        raise ValueError("Need a length axis to process an ndarray.")
+        raise ValueError(
+            "Need a length axis to process an ndarray with itemsize == 1 (S1, u1)."
+        )
 
     # OHE
     if ohe_axis is None and isinstance(seqs, np.ndarray) and seqs.dtype == np.uint8:
@@ -76,7 +80,11 @@ DTYPE = TypeVar("DTYPE", bound=np.generic)
 
 
 def array_slice(
-    a: NDArray[DTYPE], axis: int, start: int, end: int, step=1
+    a: NDArray[DTYPE],
+    axis: int,
+    start: Optional[int] = None,
+    stop: Optional[int] = None,
+    step: Optional[int] = None,
 ) -> NDArray[DTYPE]:
     """Slice an array from a dynamic axis."""
-    return a[(slice(None),) * (axis % a.ndim) + (slice(start, end, step),)]
+    return a[(slice(None),) * (axis % a.ndim) + (slice(start, stop, step),)]