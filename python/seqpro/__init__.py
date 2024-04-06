3b20997e500d8956cf383bf90f4798e64dc3f442
d-laub <dlaub@ucsd.edu>
2024-04-05 18:47:34 -0700

    bump version, remove __version__. Make Rust code have separate version from Python.

--- python/seqpro/__init__.py
+++ python/seqpro/__init__.py
@@ -7,8 +7,6 @@ from ._modifiers import bin_coverage, jitter, k_shuffle, random_seqs, reverse_co
 from ._utils import cast_seqs
 from .alphabets import AA, DNA, RNA, AminoAlphabet, NucleotideAlphabet
 
-__version__ = "0.1.12"
-
 __all__ = [
     "transforms",
     "cast_seqs",
