ee48d7fcfbf398ea2bb61a43b09bd10152106215
d-laub <dlaub@ucsd.edu>
2025-04-19 11:45:01 -0700

    fix: move length_to_offsets to _ragged

--- python/seqpro/_ragged.py
+++ python/seqpro/_ragged.py
@@ -10,8 +10,6 @@ from awkward.index import Index64
 from numpy.typing import NDArray
 from typing_extensions import Self
 
-from ._utils import lengths_to_offsets
-
 __all__ = ["Ragged"]
 
 DTYPE = TypeVar("DTYPE", bound=np.generic)
@@ -227,3 +225,24 @@ class Ragged(Generic[RDTYPE]):
                 stream=None,  # type: ignore
             ),
         )
+
+
+def lengths_to_offsets(
+    lengths: NDArray[np.integer], dtype: type[DTYPE] = OFFSET_TYPE
+) -> NDArray[DTYPE]:
+    """Convert lengths to offsets.
+
+    Parameters
+    ----------
+    lengths
+        Lengths of the segments.
+
+    Returns
+    -------
+    offsets
+        Offsets of the segments.
+    """
+    offsets = np.empty(lengths.size + 1, dtype=dtype)
+    offsets[0] = 0
+    offsets[1:] = lengths.cumsum()
+    return offsets
--- tests/test_ragged.py
+++ tests/test_ragged.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pytest_cases import parametrize_with_cases
-from seqpro._ragged import Ragged
+from seqpro._ragged import OFFSET_TYPE, Ragged, lengths_to_offsets
 
 
 def case_int32():
@@ -35,3 +35,21 @@ def test_roundtrip(rag: Ragged):
     np.testing.assert_equal(actual.data, rag.data)
     np.testing.assert_equal(actual.lengths, rag.lengths)
     np.testing.assert_equal(actual.offsets, rag.offsets)
+
+
+def l2o_1d():
+    lengths = np.array([3, 2, 5], dtype=np.uint32)
+    desired = np.array([0, 3, 5, 10], dtype=OFFSET_TYPE)
+    return lengths, desired
+
+
+def l2o_2d():
+    lengths = np.array([[3, 2], [5, 4]], dtype=np.uint32)
+    desired = np.array([0, 3, 5, 10, 14], dtype=OFFSET_TYPE)
+    return lengths, desired
+
+
+@parametrize_with_cases("lengths, desired", cases=".", prefix="l2o_")
+def test_len_to_offsets(lengths, desired):
+    actual = lengths_to_offsets(lengths)
+    np.testing.assert_equal(actual, desired)
